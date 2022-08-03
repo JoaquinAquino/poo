186d83ba75294031c93e41c63a0c74cfde07fc08
JoaquinAquino <110432887+JoaquinAquino@users.noreply.github.com>
2022-08-03 20:09:54 -0300

    joaquin9aquino987@gmail.com
    punto 5

---
+++ Ejercicio 5/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+	Scanner in = new Scanner (System.in);
+	   System.out.println("ingrese el primer valor");
+	int num1= in.nextInt();
+	   System.out.println("ingrese el segundo valor");
+	int num2= in.nextInt();
+	System.out.print(num1+num2);
+	System.out.print(num1+num2);
+	}
+
+}
